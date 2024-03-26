48cb35bff599026292932a17a9d723c3aa91166b
Andrew Ali <andrew.ali4@dcmail.ca>
2024-03-26 15:21:26 -0400

    periodic save

--- assignment-4/part-2/main.js
+++ assignment-4/part-2/main.js
@@ -5,14 +5,28 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = ['pic1', 'pic2', 'pic3', 'pic4', 'pic5'];
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i < imageArray.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/' + imageArray[i]);
+    newImage.setAttribute('alt', altText[i]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', (e) => {
+        displayedImage.src = e.target.src;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btnClass = 'light';
+    } else {
+    
+    }
+});
