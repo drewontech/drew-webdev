eccb18034224571a22430341426b44ce65fca69c
Andrew Ali <andrew.ali4@dcmail.ca>
2024-03-26 12:48:16 -0400

    Initial commit

--- assignment-4/part-3/main.js
+++ assignment-4/part-3/main.js
@@ -77,7 +77,7 @@ draw() {
 const balls = [];
 
 while (balls.length < 1000) {
-  const size = random(1, 1);
+  const size = random(0.5, 1);
   const ball = new Ball(
     // ball position always drawn at least one ball width
     // away from the edge of the canvas, to avoid drawing errors
